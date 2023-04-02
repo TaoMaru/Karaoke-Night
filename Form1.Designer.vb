2ee91fad61c64bbcf9d72c149ccb9ae9a9bac38a
TaoMaru <jackson_maria1@columbusstate.edu>
2023-04-02 02:17:25 -0400

    Add new blank splash screen & resize

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class fmKaraoke
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,9 +20,9 @@ Partial Class fmKaraoke
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(fmKaraoke))
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.picKaraoke = New System.Windows.Forms.PictureBox()
         Me.lblHeading = New System.Windows.Forms.Label()
         Me.lblInstructions = New System.Windows.Forms.Label()
--- Form1.vb
+++ Form1.vb
@@ -4,7 +4,7 @@
 ' Purpose: This app lets users choose a karaoke room rental type and calculate their total cost
 ''          Users select to rent per song or sent a room by hour, and their cost is displayed
 
-Public Class fmKaraoke
+Public Class Form1
     Private Sub fmKaraoke_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' Pauses the splash screen after 3 seconds
         Threading.Thread.Sleep(3000)
